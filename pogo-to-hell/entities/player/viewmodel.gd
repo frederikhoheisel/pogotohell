d438015ec7061ad4b5e9b66491d04df66b24e387
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-11-11 20:08:21 +0100

    laptop commit damit ich am pc weitermachen kann :/

--- pogo-to-hell/entities/player/viewmodel.gd
+++ pogo-to-hell/entities/player/viewmodel.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 
 @onready var animation_player: AnimationPlayer = $arm_view_model/AnimationPlayer
+@onready var revolver: Node3D = $arm_view_model/Arm_Rig/Skeleton3D/BoneAttachment3D/revolver
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,5 +17,10 @@ func play_fire_anim() -> void:
 
 
 func play_pull_out_anim() -> void:
+	var tween: Tween = get_tree().create_tween()
+	#tween.set_parallel(false)
+	tween.tween_property(revolver, "rotation", revolver.rotation + Vector3(2.0 * PI, 0.0, 0.0), 0.5)
+	#tween.tween_property(revolver, "rotation", revolver.rotation + Vector3(2.0 * PI, 0.0, 0.0), 0.25)
+	
 	animation_player.play("ArmViewModel/view_model_pull_out")
 	animation_player.queue("ArmViewModel/view_model_idle")
