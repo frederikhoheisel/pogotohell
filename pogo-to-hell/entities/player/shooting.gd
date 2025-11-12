d9975d8ca430c9f3387b0107b22f2e57110379e8
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-11-12 20:50:51 +0100

    bessere gun decals (mit pool)

--- pogo-to-hell/entities/player/shooting.gd
+++ pogo-to-hell/entities/player/shooting.gd
@@ -44,14 +44,6 @@ func _shake_camera() -> void:
 	camera_tween.tween_property(head, "rotation", Vector3(head.rotation.x, head.rotation.y, head.rotation.z), camera_shake_duration).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 
 
-func _place_decal(collision_point: Vector3, collision_normal: Vector3) -> void:
-	var decal_scene: Decal = decal.instantiate()
-	get_tree().root.get_child(0).add_child(decal_scene)
-	decal_scene.global_position = collision_point
-	decal_scene.look_at(collision_point - collision_normal)
-	decal_scene.rotation.x += PI/2.0
-
-
 func shoot_gun() -> void:
 	_shake_camera()
 	view_model.play_fire_anim()
@@ -63,7 +55,7 @@ func shoot_gun() -> void:
 		var collision_point: Vector3 = hit_ray_cast.get_collision_point()
 		var collision_normal: Vector3 = hit_ray_cast.get_collision_normal()
 		
-		_place_decal(collision_point, collision_normal)
+		get_tree().get_first_node_in_group("DecalManager").place_decal(collision_point, collision_normal)
 		
 		if collider is SoftBody3D:
 			collider.apply_central_impulse(collision_normal * -10.0)
